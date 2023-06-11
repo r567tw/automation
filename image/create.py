4a01dfc52e0c6e388268a8b124e68b618ba1ed07
r567tw <r567tw@gmail.com>
2023-06-12 05:49:36 +0800

    update image/create.py

--- image/create.py
+++ image/create.py
@@ -1,7 +1,23 @@
 from PIL import Image  
+import webcolors
 
 width = 400
 height = 300
 
-img  = Image.new( mode = "RGB", size = (width, height), color = (255, 255, 0) )
-img.save("yellow.gif")
+def color_name_to_rgb(color_name):
+    try:
+        color = webcolors.name_to_rgb(color_name)
+        return color
+    except ValueError:
+        return None
+
+color_name = input("請輸入顏色名稱：")
+
+rgb_color = color_name_to_rgb(color_name)
+
+
+if rgb_color is not None:
+    img  = Image.new( mode = "RGB", size = (width, height), color = (rgb_color[0], rgb_color[1], rgb_color[2]) )
+    img.save("{}.gif".format(color_name))
+else:
+    print("Not Valid Color")
